e23069800cbac885af278e010881580c7f37c80a
Nidheesh <nilu.1999@yahoo.com>
2024-09-23 14:57:26 +0000

    v0.8 update

--- vrl-lambda.sh
+++ vrl-lambda.sh
@@ -1,10 +1,11 @@
 set -e
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
 export REPO_NAME="cypienta-vrl-lambda"
-docker pull public.ecr.aws/p2d2x2s3/cypienta/vrl-lambda:v0.1
+export VRL_TAG="v0.2"
+docker pull public.ecr.aws/p2d2x2s3/cypienta/vrl-lambda:${VRL_TAG}
 aws ecr create-repository --repository-name ${REPO_NAME}
 export ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_URI}
-docker tag public.ecr.aws/p2d2x2s3/cypienta/vrl-lambda:v0.1 ${ECR_URI}/${REPO_NAME}:v0.1
-docker push ${ECR_URI}/${REPO_NAME}:v0.1
-echo ${ECR_URI}/${REPO_NAME}:v0.1
+docker tag public.ecr.aws/p2d2x2s3/cypienta/vrl-lambda:${VRL_TAG} ${ECR_URI}/${REPO_NAME}:${VRL_TAG}
+docker push ${ECR_URI}/${REPO_NAME}:${VRL_TAG}
+echo ${ECR_URI}/${REPO_NAME}:${VRL_TAG}
