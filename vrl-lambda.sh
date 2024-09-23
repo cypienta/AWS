894cb84920467a47ce8829ed89ad8dd999c054c2
Nidheesh <nilu.1999@yahoo.com>
2024-09-23 20:14:44 +0000

    update vrl lambda shell script

--- vrl-lambda.sh
+++ vrl-lambda.sh
@@ -3,7 +3,15 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output t
 export REPO_NAME="cypienta-vrl-lambda"
 export VRL_TAG="v0.2"
 docker pull public.ecr.aws/p2d2x2s3/cypienta/vrl-lambda:${VRL_TAG}
-aws ecr create-repository --repository-name ${REPO_NAME}
+# Check if the repository already exists
+aws ecr describe-repositories --repository-names "${REPO_NAME}" >/dev/null 2>&1
+# If the repository doesn't exist (exit status is not 0), create it
+if [ $? -ne 0 ]; then
+    echo "Repository does not exist. Creating repository: ${REPO_NAME}"
+    aws ecr create-repository --repository-name ${REPO_NAME}
+else
+    echo "Repository ${REPO_NAME} already exists. Skipping creation."
+fi
 export ECR_URI="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ECR_URI}
 docker tag public.ecr.aws/p2d2x2s3/cypienta/vrl-lambda:${VRL_TAG} ${ECR_URI}/${REPO_NAME}:${VRL_TAG}
