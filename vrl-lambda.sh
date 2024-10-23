73eb6b49b0ab4971659be702020d222e0b49866b
Nidheesh Panchal <33429560+Nidheesh-Panchal@users.noreply.github.com>
2024-10-23 09:57:38 -0400

    bug fix script

--- vrl-lambda.sh
+++ vrl-lambda.sh
@@ -3,10 +3,8 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output t
 export REPO_NAME="cypienta-vrl-lambda"
 export VRL_TAG="v0.2"
 docker pull public.ecr.aws/p2d2x2s3/cypienta/vrl-lambda:${VRL_TAG}
-# Check if the repository already exists
-aws ecr describe-repositories --repository-names "${REPO_NAME}" >/dev/null 2>&1
 # If the repository doesn't exist (exit status is not 0), create it
-if [ $? -ne 0 ]; then
+if ! aws ecr describe-repositories --repository-names "${REPO_NAME}" >/dev/null 2>&1; then
     echo "Repository does not exist. Creating repository: ${REPO_NAME}"
     aws ecr create-repository --repository-name ${REPO_NAME}
 else
